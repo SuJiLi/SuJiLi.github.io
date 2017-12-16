41aaf4afd3e86f294cc041789752589f3f81e056
sujili <840661624@qq.com>
2017-12-16 17:18:28 +0800

    add all 2017.12.16 17:18

--- js/index.js
+++ js/index.js
@@ -1,22 +1,10 @@
 $(function(){
     $('.con').fullpage({
-        anchors:['page1','page2','page3'],
+        anchors:['page1','page2','page3','page4'],
         menu:'#menu',
         verticalCentered:false,
     });
 
-    $(window).resize(function () {
-       autoScrolling();
-    });
-    function autoScrolling() {
-        var $ww=$(window).width();
-        if($ww<1024){
-            $.fn.fullpage.setAutoScrolling(false);
-        }else {
-            $.fn.fullpage.setAutoScrolling(true);
-        }
-    }
-    autoScrolling();
 });
 
 $(document).ready({
