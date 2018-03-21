163ec4647a01a74ff79ed1d3417b5c69d043800d
SuJiLi <840661624@qq.com>
2018-03-21 20:28:07 +0800

    2018.3.21

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function(){
     $('.con').fullpage({
-        anchors:['page1','page2','page3','page4'],
+        anchors:['page1','page2','page3','page4','page5','page6'],
         menu:'#menu',
         verticalCentered:false,
     });
